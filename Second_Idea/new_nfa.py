836578614d964f0365a8a0d330f28395a98c0c9e
David Sackler <davidsackler@Davids-MacBook-Pro.local>
2020-12-10 15:38:10 -0800

    Finished dfa converter and added tests

--- Second_Idea/new_nfa.py
+++ Second_Idea/new_nfa.py
@@ -1,6 +1,4 @@
-
 import json
-EPSILON = ""
 
 
 class NFA:
@@ -10,68 +8,84 @@ class NFA:
         self.start = description['start']
 
     def is_accept(self, string):
+        for symbol in string:
+            if symbol in self.accept_states:
+                return True
         return False
-        # return bool
 
     def get_lambda_moves(self, state):
         for label in self.transitions[state].keys():
             if label == 'λ':
-                return self.transitions[state]['λ']
+                return set(self.transitions[state]['λ'])
+            else:
+                continue
+        return set()
 
     def transition(self, state, symbol):
-        # deal with nulls. return empty list
         if state in self.transitions and symbol in self.transitions[state]:
-            return self.transitions[state][symbol]
+            return set(self.transitions[state][symbol])
         else:
-            return []
-            # return set of states
+            return set()
+    
+
 
 
 def to_dfa(nfa):
     dfa_description = {'transitions': {}, 'accept_states': [], 'start': ''}
-    # first, get all lambda moves from each state
-    lambda_moves_from_states = []
-    for state in nfa.transitions.keys():
-        if 'λ' in nfa.transitions[state].keys():
-            lambda_moves_from_states.append(
-                (state, NFA.get_lambda_moves(nfa, state)))
 
     # 1) get start state
     start_state = nfa.start
     if 'λ' in nfa.transitions[start_state].keys():
-        start_state = start_state + ''.join(nfa.transitions[start_state]['λ']) # the ''.join turns list elements into a string
-    dfa_description['start'] = start_state
+        start_state = start_state + ''.join(nfa.transitions[start_state]['λ'])
+    dfa_description['start'] = ''.join(sorted(start_state))
 
     # 2) get transitions from start state
     no_repeats = []
     todo = [start_state]
     while len(todo) != 0:
-        current = todo.pop()
+        current = ''.join(sorted(todo.pop()))
         if current in no_repeats:
             continue
         no_repeats.append(current)
-        current_transition = {current: {'0': '', '1': ''}}
-        for state in current:
-            zero_label = ''.join(list(set(current_transition[current]['0'] + ''.join(NFA.transition(nfa, state, '0')))))
-            one_label = ''.join(list(set(current_transition[current]['1'] + ''.join(NFA.transition(nfa, state, '1')))))
-            current_transition[current]['0'] = zero_label
-            current_transition[current]['1'] = one_label
-            todo.append(zero_label)
-            todo.append(one_label)
-            dfa_description['transitions'].update(current_transition) #this is just to test it. Dont update dfa_description until you get lambda moves from each state
-    #print(f'{dfa_description} + dfa transition')
-    print(json.dumps(dfa_description, indent=4, sort_keys=False))
-        
-    # 3) get lambda moves from each transition
-    # for index in dfa_description['transitions'][start_transition]['0']:
-    #     for state in lambda_moves_from_states:
-    #         if dfa_description['transitions']['SAC']['0'][0] == lambda_moves_from_states[state][0]:
-    #             dfa_description['transitions'][start_transition]['0'] = dfa_description['transitions'][start_transition]['0'] + lambda_moves_from_states[state][0]
-    #         if dfa_description['transitions']['SAC']['1'][0] == lambda_moves_from_states[state][0]:
-    #             dfa_description['transitions'][start_transition]['1'] = dfa_description['transitions'][start_transition]['1'] + lambda_moves_from_states[state][0]
+        current_transition = {current: {'0': set(), '1': set()}}
+        for state in current: 
+            #Get 1 and 0 transitions 
+            current_transition[current]['0'] = current_transition[current]['0'].union(NFA.transition(nfa, state, '0'))
+            current_transition[current]['1'] = current_transition[current]['1'].union(NFA.transition(nfa, state, '1'))
+            #get lambda moves
+            lambda_set = set()
+            for state in current_transition[current]['0']:
+                lambda_set = lambda_set.union(NFA.get_lambda_moves(nfa, state))
+            current_transition[current]['0'] = current_transition[current]['0'].union(lambda_set)
+            lambda_set.clear()
+            for state in current_transition[current]['1']:
+                lambda_set = lambda_set.union(NFA.get_lambda_moves(nfa, state))
+            current_transition[current]['1'] = current_transition[current]['1'].union(lambda_set)
+            
+            dfa_description['transitions'].update(current_transition)
+        todo.append(''.join(current_transition[current]['0']))
+        todo.append(''.join(current_transition[current]['1']))
 
-    # dfa_description['transitions'] = start_transition
-    return lambda_moves_from_states
+    #Figure out which states are accept states
+    for state in dfa_description['transitions']:
+        if NFA.is_accept(nfa, state) == True:
+            dfa_description['accept_states'].append(''.join(sorted(state)))
+        
+
+    #Sort key names and value names
+    for state in dfa_description['transitions'].keys():
+        
+        dfa_description['transitions'][state]['0'] = ''.join(sorted(dfa_description['transitions'][state]['0']))
+        if dfa_description['transitions'][state]['0'] == "":
+            dfa_description['transitions'][state]['0'] = "Dead state"
+        dfa_description['transitions'][state]['1'] = ''.join(sorted(dfa_description['transitions'][state]['1']))
+        if dfa_description['transitions'][state]['1'] == "":
+            dfa_description['transitions'][state]['1'] = "Dead state"
+
+    print(json.dumps(dfa_description, indent=4, sort_keys=False))
+    return dfa_description
+
+
 
 
 # Get accept funciton working
--- Second_Idea/nfa_test.py
+++ Second_Idea/nfa_test.py
@@ -23,7 +23,7 @@ test_machines = {
                     'λ': ['A', 'C']
                 },
                 'A': {
-                    '0': ['B', 'C'],
+                    '0': ['B'],
                     '1': ['A']
                 },
                 'C': {
@@ -44,18 +44,199 @@ test_machines = {
         },
         'accepts': [],
         'rejects': ['01', '10']
+    },
+    'machine1': {
+        'description': {
+            'transitions': {
+                'H': {
+                    '0': ['I']
+                },
+                'I': {
+                    '0': ['J'],
+                    'λ': ['K']
+                },
+                'J': {
+                    '0': ['K']
+                },
+                'K': {
+                    '0': ['K'],
+                    '1': ['K']
+                }
+                
+            },
+            'accept_states': ['K'],
+            'start': 'H'
+        },
+        'accepts': [],
+        'rejects': []
+    },
+    'machine2': {
+        'description': {
+            'transitions': {
+                'A': {
+                    '1': ['B']
+                },
+                'B': {
+                    '0': ['B'],
+                    '1': ['B', 'C']
+                },
+                'C': {
+                    
+                }
+                
+            },
+            'accept_states': ['C'],
+            'start': 'A'
+        },
+        'accepts': [],
+        'rejects': []
+    },
+    'machine3': {
+        'description': {
+            'transitions': {
+                'A': {
+                    'λ': ['E', 'F', 'B']
+                },
+                'E': {
+                    '0': ['E'],
+                    '1': ['F']
+                },
+                'F': {
+                    '0': ['E'],
+                    '1': ['F']
+                },
+                'B': {
+                    '0': ['B'],
+                    '1': ['C']
+                },
+                'C': {
+                    '0': ['B'],
+                    '1': ['C']
+                }
+            },
+            'accept_states': ['C', 'E'],
+            'start': 'A'
+        },
+        'accepts': [],
+        'rejects': []
     }
 }
 
 
 def test_transition():
     nfa = NFA(test_machines['div2']['description'])
-    assert nfa.transition('S', '1') == ['S']
-    assert nfa.transition('A', '0') == []
+    assert nfa.transition('S', '1') == {'S'}
+    assert nfa.transition('A', '0') == set()
 
     # VIM bindings
 
 
 def test_lambda():
     nfa = NFA(test_machines['lambda_test']['description'])
-    assert to_dfa(nfa) == [('S', ['A', 'C'])]
+    assert to_dfa(nfa) == {
+    "transitions": {
+        "ACS": {
+            "0": "BC",
+            "1": "AD"
+        },
+        "AD": {
+            "0": "BD",
+            "1": "AC"
+        },
+        "AC": {
+            "0": "BC",
+            "1": "AD"
+        },
+        "BC": {
+            "0": "AC",
+            "1": "BD"
+        },
+        "BD": {
+            "0": "AD",
+            "1": "BC"
+        }
+    },
+    "accept_states": [
+        "ACS",
+        "AD",
+        "AC",
+        "BC"
+    ],
+    "start": "ACS"
+}
+
+def test_machine1():
+    nfa = NFA(test_machines['machine1']['description'])
+    assert to_dfa(nfa) == {"transitions": {
+            "H": {
+                "0": "IK",
+                "1": "Dead state"
+            },
+            "IK": {
+                "0": "JK",
+                "1": "K"
+            },
+            "K": {
+                "0": "K",
+                "1": "K"
+            },
+            "JK": {
+                "0": "K",
+                "1": "K"
+            }
+        },
+        "accept_states": [
+            "IK",
+            "K",
+            "JK"
+        ],
+        "start": "H"
+    }
+
+def test_machine2():
+    nfa = NFA(test_machines['machine2']['description'])
+    assert to_dfa(nfa) == {"transitions": {
+            "A": {
+                "0": "Dead state",
+                "1": "B"
+            },
+            "B": {
+                "0": "B",
+                "1": "BC"
+            },
+            "BC": {
+                "0": "B",
+                "1": "BC"
+            }
+        },
+        "accept_states": [
+            "BC"
+        ],
+        "start": "A"
+    }
+
+def test_machine3():
+    nfa = NFA(test_machines['machine3']['description'])
+    assert to_dfa(nfa) == {"transitions": {
+            "ABEF": {
+                "0": "BE",
+                "1": "CF"
+            },
+            "CF": {
+                "0": "BE",
+                "1": "CF"
+            },
+            "BE": {
+                "0": "BE",
+                "1": "CF"
+            }
+            
+        },
+        "accept_states": [
+            "ABEF",
+            "CF",
+            "BE"
+            
+        ],
+        "start": "ABEF"
+    }
